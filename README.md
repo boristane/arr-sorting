806fa34c17dd43c6ecc4843d1594f885ce88ff10
Boris Tane <boris.tane@gmail.com>
2018-11-24 09:44:25 +0000

    Perform merge sort (#3)

--- README.md
+++ README.md
@@ -1,5 +1,35 @@
 # arr-sorting
 
-Sort an array using various popular sorting algorithms.
+Sort an array using various popular sorting algorithms in javascript.
 
-[![Build Status](https://travis-ci.org/boristane/arr-sorting.svg?branch=master)](https://travis-ci.org/boristane/arr-sorting)
+[![Build Status](https://travis-ci.org/boristane/arr-sorting.svg?branch=master)](https://travis-ci.org/boristane/arr-sorting) [![Codacy Badge](https://api.codacy.com/project/badge/Grade/6f4ef9df6de64d15b32d824c89b05b5e)](https://www.codacy.com/app/boris.tane/arr-sorting?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=boristane/arr-sorting&amp;utm_campaign=Badge_Grade) [![tested with jest](https://img.shields.io/badge/tested_with-jest-99424f.svg)](https://github.com/facebook/jest) [![jest](https://jestjs.io/img/jest-badge.svg)](https://github.com/facebook/jest)
+
+## Installation
+
+## Documentation
+
+The library contains a set of functions perfroming array sorting using various sorting algorithms.
+
+### bubble
+Sorts an array according to a compare function using the bubble sort algorithm.
+```js
+const arr = [2, 5, 10, 5, 32, 6];
+const arrObjects = [ { a: 5, }, { a: 1, }, { a: 4, }, { a: 2, }, { a: 8, }, ];
+bubble(arr); // [2, 5, 5, 6, 10, 32]
+bubble(arr, (a, b) => b - a); // [32, 10, 6, 5, 5, 2]
+bubble(arrObjects, (obj1, obj2) => obj1.a - obj2.a); // [ { a: 1, }, { a: 2, }, { a: 4, }, { a: 5, }, { a: 8, } ]
+```
+
+### merge
+Sorts an array according to a compare function using the merge sort algorithm.
+```js
+const arr = [2, 5, 10, 5, 32, 6];
+const arrObjects = [ { a: 5, }, { a: 1, }, { a: 4, }, { a: 2, }, { a: 8, }, ];
+merge(arr); // [2, 5, 5, 6, 10, 32]
+merge(arr, (a, b) => b - a); // [32, 10, 6, 5, 5, 2]
+merge(arrObjects, (obj1, obj2) => obj1.a - obj2.a); // [ { a: 1, }, { a: 2, }, { a: 4, }, { a: 5, }, { a: 8, } ]
+```
+
+## Licence
+
+The library is [MIT licenced](LICENCE).
--- test/sorting.test.js
+++ test/sorting.test.js
@@ -1,59 +1,43 @@
 import bubble from '../src/bubble';
+import merge from '../src/merge';
+
 const sorting = [
   {
     name: 'bubble',
     algo: bubble,
   },
+  {
+    name: 'merge',
+    algo: merge,
+  },
 ];
+
+const arrObjects = [ { a: 5, }, { a: 1, }, { a: 4, }, { a: 2, }, { a: 8, }, ];
+const sortedArrObjects = [ { a: 1, }, { a: 2, }, { a: 4, }, { a: 5, }, { a: 8, }, ];
+const arrInt = [ 5, 1, 4, 2, 8, ];
+const sortedArrInt = [ 1, 2, 4, 5, 8, ];
+const descendingOrderSortedArrInt = [ 8, 5, 4, 2, 1, ];
+const obj = { a: 1, b: 4, };
 
 sorting.forEach(algo => {
   describe(`${algo.name} sort`, () => {
     test('on array of int', () => {
-      expect(algo.algo([5, 1, 4, 2, 8], (a, b) => a - b)).toEqual([1, 2, 4, 5, 8]);
+      expect(algo.algo(arrInt, (a, b) => a - b)).toEqual(sortedArrInt);
+    });
+    test('in descending order', () => {
+      expect(algo.algo(arrInt, (a, b) => b - a)).toEqual(descendingOrderSortedArrInt);
     });
     test('on array of objects', () => {
-      expect(algo.algo([
-        {
-          a: 5,
-        },
-        {
-          a: 1,
-        },
-        {
-          a: 4,
-        },
-        {
-          a: 2,
-        },
-        {
-          a: 8,
-        },
-      ], (obj1, obj2) => obj1.a - obj2.a)).toEqual([
-        {
-          a: 1,
-        },
-        {
-          a: 2,
-        },
-        {
-          a: 4,
-        },
-        {
-          a: 5,
-        },
-        {
-          a: 8,
-        },
-      ]);
+      expect(algo.algo(arrObjects, (obj1, obj2) => obj1.a - obj2.a)).toEqual(sortedArrObjects);
     });
     test('without second argument', () => {
-      expect(algo.algo([5, 1, 4, 2, 8])).toEqual([1, 2, 4, 5, 8]);
+      expect(algo.algo(arrInt)).toEqual(sortedArrInt);
     });
     test('with dummy function', () => {
-      expect(algo.algo([5, 1, 4, 2, 8], 1)).toEqual([5, 1, 4, 2, 8]);
+      expect(algo.algo(arrInt, 1)).toEqual(arrInt);
     });
     test('with non array as first argument', () => {
-      expect(algo.algo({a: 1, b: 4})).toBeUndefined();
+      expect(algo.algo(obj)).toBeUndefined();
     });
   });
 });
