3b02f46bd7b2b19418591291c09658da8449a7da
Boris Tane <boris.tane@gmail.com>
2018-12-04 23:59:16 +0000

    Implement timsort

--- src/merge.js
+++ src/merge.js
@@ -1,22 +1,5 @@
 import * as utils from './utils';
-
-function arrayMerge(left, right, compFunc) {
-  const result = [];
-  let leftIdx = 0;
-  let rightIdx = 0;
-  while (leftIdx < left.length && rightIdx < right.length) {
-    if (compFunc(left[leftIdx], right[rightIdx]) <= 0) {
-      result.push(left[leftIdx]);
-      leftIdx += 1;
-    } else {
-      result.push(right[rightIdx]);
-      rightIdx += 1;
-    }
-  }
-  result.push(...left.slice(leftIdx));
-  result.push(...right.slice(rightIdx));
-  return result;
-}
+import arrayMerge from './arrayMerge';
 
 /**
  * Sorts an array of objects according to a
